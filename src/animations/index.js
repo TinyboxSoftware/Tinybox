7d5f0bf1585d1980cad2ba830f31f704acb84876
Mykal Machon <mykal@tinybox.dev>
2022-04-14 12:00:19 -0700

    feat: fixed animation imports

---
+++ src/animations/index.js
@@ -0,0 +1,19 @@
+import { gsap } from "gsap";
+
+const initTl = gsap.timeline({});
+initTl.fromTo(
+  "small",
+  { opacity: 0, y: -10 },
+  { opacity: 1, y: 0, duration: 0.4 }
+);
+initTl.fromTo(
+  "h1",
+  { opacity: 0, y: -10 },
+  { opacity: 1, y: 0, duration: 0.4 }
+);
+initTl.fromTo(
+  "#datetime",
+  { opacity: 0, y: 5 },
+  { opacity: 1, y: 0, duration: 0.8 },
+  "<0.4"
+);
