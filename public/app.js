a441192c95aff96f14150baf67f0649bcbb2de78
Mykal Machon <mykal@tinybox.dev>
2022-04-17 11:49:46 -0700

    feat: ugly theme toggler

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 // set themes
 const siteContainerEl = document.querySelector("div.site-container");
+const siteThemeBtnEl = document.querySelector('button#theme');
 const themePref = localStorage.getItem("theme");
 
 if (themePref && themePref === "night") {
@@ -14,10 +15,28 @@ if (!themePref) {
     window.matchMedia("(prefers-color-scheme: dark)").matches
   ) {
     siteContainerEl.classList.add("theme__night");
+    localStorage.setItem('theme', 'night');
   } else {
     siteContainerEl.classList.add("theme__day");
+    localStorage.setItem('theme', 'day');
   }
 }
+
+siteThemeBtnEl.addEventListener('click',(event) => {
+  event.preventDefault();
+  const currTheme = localStorage.getItem('theme');
+  console.log('doing things')
+  if(currTheme === 'night'){
+    localStorage.setItem('theme', 'day');
+    siteContainerEl.classList.add('theme__day');
+    siteContainerEl.classList.remove('theme__night');
+  } else {
+    localStorage.setItem('theme', 'night');
+    siteContainerEl.classList.add('theme__night')
+    siteContainerEl.classList.remove('theme__day');
+  }
+})
+
 
 // to date stuff in footer
 const datetimeEl = document.querySelector("#datetime");
