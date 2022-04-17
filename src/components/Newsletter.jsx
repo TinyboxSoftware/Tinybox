a99a7634c0d2b468bd14f44166a39d313012b271
Mykal Machon <mykal@tinybox.dev>
2022-04-17 11:28:35 -0700

    feat: cleaned stuff up

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,30 +1,37 @@
-import { useEffect, useState } from 'preact/hooks';
+import { useEffect, useState } from "preact/hooks";
 
 const Newsletter = () => {
   const [isInit, setIsInit] = useState(false);
 
   const onSubmit = (event) => {
     event.preventDefault();
-    console.log('submitted')
-  }
+    console.log("submitted");
+  };
 
   useEffect(() => {
     setIsInit(true);
-  }, [])
+  }, []);
 
   return (
-    <form method="POST" action="/api/newsletter" onSubmit={onSubmit}>
-      <div class="form-control">
-        <label for="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="johndoe@gmail.com"
-        />
-      </div>
-      <button disabled={!isInit}>Signup</button>
-    </form>
+    <section>
+      <h2>Newsletter</h2>
+      <p>
+        if you want to keep up with what Tinybox is up to, signup to the
+        newsletter.
+      </p>
+      <form method="POST" action="/api/newsletter" onSubmit={onSubmit}>
+        <div class="form-control">
+          <label for="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="johndoe@gmail.com"
+          />
+        </div>
+        <button disabled={!isInit}>Signup</button>
+      </form>
+    </section>
   );
 };
 
